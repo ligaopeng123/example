a8bbb7b46f3e849849229a4144ac9d1e2f6061b6
ligaopeng123 <gaopeng@123>
2021-06-22 08:48:13 +0800

    pipe

--- functional/compose.ts
+++ functional/compose.ts
@@ -33,7 +33,7 @@ export const compose = (...fns: any) => {
 	if (fns.length === 0) return (arg: any) => arg;
 	if (fns.length === 1) return fns[0];
 	/**
-	 * 巧妙利用reduce的一直拼接特性
+	 * 巧妙利用reduce的一直拼接特性 函数从右到左执行 右边的函数结果做完参数传递下去
 	 * 返回一个无限包裹的函数(x)=> a(b(c(c(d(x)))))
 	 */
 	return fns.reduce((a: any, b: any) => (...args: any) => a(b(...args)))
---
+++ functional/pipe.ts
@@ -0,0 +1,12 @@
+/**
+ * compose 函数
+ */
+export const pipe = (...fns: any) => {
+	if (fns.length === 0) return (arg: any) => arg;
+	if (fns.length === 1) return fns[0];
+	/**
+	 * 巧妙利用reduce的一直拼接特性 从左到右执行
+	 * 返回一个无限包裹的函数(x)=> d(c(b(a(x))))
+	 */
+	return fns.reduce((a: any, b: any) => (...args: any) => b(a(...args)))
+};
