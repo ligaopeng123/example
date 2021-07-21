e186b55f2dbcc9250f0016c4bd81376bd9f1810f
ligaopeng123 <gaopeng@123>
2021-07-21 08:40:12 +0800

    io monad

--- typescript/CPS.ts
+++ typescript/CPS.ts
@@ -2,15 +2,75 @@
  *
  * @模块名称: CPS
  *
- * @模块用途: CPS风格
+ * @模块用途: CPS
  *
- * @date: 2021/7/19 14:21
+ * @date: 2021/7/20 21:08
  *
  * @版权所有: pgli
  *
  **********************************************************************/
-class CPS {
+type Fn = (...args: Array<number>) => number
+const addFn: Fn = (x, y) => x + y;
+const square: Fn = (z) => z * z;
+// 一般写法
+square(addFn(2, 3));
+// Continuation-passing style
+const cps = (x, y, square) => square(x + y);
 
-}
+/**
+ * 普通递归
+ * @param num
+ * @returns {*}
+ */
+export const cps1 = (num) => {
+	if (num < 1) return num;
+	return num + cps1(num - 1);
+};
+/**
+ * cps 尾递归
+ * @param num
+ * @returns {*}
+ */
+export const cps2 = (num, sum = 1) => {
+	if (num < 1) return sum;
+	// cps2就是个cps风格代码
+	return cps2(num - 1, sum + num);
+};
 
-export default CPS;
+/**
+ * 递归转迭代
+ * @constructor
+ */
+const Iteration = (fn) => {
+	let v = 0;
+	// 状态管理 返回undefined 避免产生调用帧
+	let active = false;
+	const args = [];
+	const f = (...args2) => {
+		args.push(args2);
+		if (!active) {
+			active = true;
+			while (args.length) {
+				v = fn(...args.shift());
+			}
+			active = false;
+			return v;
+		}
+	};
+	return f;
+};
+
+/**
+ * 迭代
+ * @type {f}
+ */
+export const cps3 = Iteration((num, sum) => {
+	if (num < 1) return sum;
+	return cps3(num - 1, sum + num);
+});
+/**
+ * chrome测试
+ */
+// console.log(cps1(10466))
+// console.log(cps2(8373))
+// console.log(cps3(20000, 1))
--- typescript/IOMonad.ts
+++ typescript/IOMonad.ts
@@ -9,8 +9,86 @@
  * @版权所有: pgli
  *
  **********************************************************************/
-class IOMonad {
-	
+/**
+ * 读取localStorage数据，将其绘制到dom结构体上，代码如下
+ */
+function getUserData() {
+	return localStorage.getItem('userInfo');
 }
+function writeDom(text) {
+	document.body.innerHTML = text;
+}
+function setUser() {
+	// 副作用 无法保证userData的值是否合法
+	const userData = getUserData();
+	const userInfo = JSON.parse(userData);
+	const username = userInfo.username;
+	writeDom(username );
+}
+/**
+ * 处理dom操作 保证操作的纯粹
+ */
+export default class IOMonad {
+	effect = null;
+	
+	constructor(effect) {
+		if (typeof effect !== 'function') {
+			throw `${effect} not a function!`
+		}
+		this.effect = effect;
+	}
+	
+	static of(v) {
+		return new IOMonad(() => v);
+	}
+	
+	static form(fn) {
+		return new IOMonad(fn);
+	}
+	
+	/**
+	 * 将副作用函数转换
+	 * @param fn
+	 * @returns {IOMonad}
+	 */
+	map(fn) {
+		return new IOMonad(() => {
+			try {
+				return fn(this.effect())
+			} catch (e) {
+				console.error(e);
+			}
+		})
+	}
+	
+	/**
+	 * 将数据平铺
+	 * @param fn
+	 * @returns {*}
+	 */
+	chain(fn) {
+		return fn(this.effect());
+	}
+	
+	/**
+	 * 执行副作用函数
+	 */
+	run() {
+		return this.effect();
+	}
+	
+	/**
+	 * 执行副作用
+	 * @param fn
+	 */
+	fork(fn) {
+		fn && typeof fn == 'function' && fn(this.run());
+	}
+};
 
-export default IOMonad;
+// 包裹函数，此处没有触发执行时机，函数不会执行
+const writeUsername = IOMonad.form(getUserData).map(JSON.parse).map(x => x.username);
+console.log(1); // 正常打印
+// 触发函数执行，开始执行包裹的函数
+writeUsername.fork(writeDom);
+console.log(2); // 正常打印 即使有错误
